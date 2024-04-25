6aaf629508b9a4823b2cfb4efb15b8cf1927a0c3
Ambati Sathish Kumar Reddy <ambatisathishkumarreddy@gmail.com>
2024-04-25 20:59:03 +0530

    adding all files

--- mysql-installation.sh
+++ mysql-installation.sh
@@ -5,19 +5,15 @@ source ./common.sh
 check_root
 
 echo "Please enter DB password:"
-read  mysql_root_password
-echo "$mysql_root_password"
+read -s mysql_root_password
 
 dnf install mysql-server -y &>>$Logfile
-echo "1"
 
 systemctl enable mysqld &>>$Logfile
-echo "2"
 
 systemctl start mysqld &>>$Logfile
-echo "2"
 
-mysql -h db.daws78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$Logfile
+mysql -h db.sathishreddy.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$Logfile
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$Logfile
